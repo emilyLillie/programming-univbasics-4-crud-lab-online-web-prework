67e519de42622e8d77a30a64551c007566a39e94
Emily Lillie <emilylillie3@gmail.com>
2019-06-18 21:23:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_cats = ["Rusty", "Gandalf", "Kitten", "Leo"]
+  $my_cats = ["Rusty", "Gandalf", "Kitten", "Leo"]
 end
 
 def add_element_to_end_of_array(array, element)
