d7875e0274c35268c615757f507c331696ce1c4a
Emily Lillie <emilylillie3@gmail.com>
2019-06-18 21:25:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  $my_array = 
+  $my_array = ["wow", "I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
-  my_cats.push("Tigger")
+  my_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
