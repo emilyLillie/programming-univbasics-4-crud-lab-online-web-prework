9e534ca7d9cb4724fa47eee1fca982fecec55438
Emily Lillie <emilylillie3@gmail.com>
2019-06-18 21:37:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,8 +14,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
-  maru_cat = famous_cats.pop
+  
 end
 
 def remove_element_from_end_of_array(array)
